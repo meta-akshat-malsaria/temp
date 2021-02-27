1dc07a5022a6bfd55ccfa3e049446b41d072bfb2
Akshat Malsaria <akshat.malsaria@metacube.com>
2021-02-28 02:56:52 +0530

    Comments added and access modifiers are mentioned.

--- Shopping/src/ShoppingCart.java
+++ Shopping/src/ShoppingCart.java
@@ -4,22 +4,33 @@ public class ShoppingCart {
 	HashMap<String,Integer> hashPrice= new HashMap<String,Integer>();
 	HashMap<String,Integer> hashQuantity= new HashMap<String,Integer>();
 	
-	
-	void addToCart(String nameOfItem ,int priceOfItem, int quantityOfItem){
+	/**
+	 * addToCart method is used to add an item to the cart
+	 */
+	public void addToCart(String nameOfItem ,int priceOfItem, int quantityOfItem){
 		hashPrice.put(nameOfItem,priceOfItem);
 		hashQuantity.put(nameOfItem,quantityOfItem);
 	}
 	
-	void updateCart(String nameOfItem,int updatedQuantity){
+	/**
+	 * updateCart method is used to update on to the quantity of an individual item.
+	 */
+	public void updateCart(String nameOfItem,int updatedQuantity){
 		hashQuantity.replace(nameOfItem,updatedQuantity);
 	}
 	
-	void removeItem(String nameOfItem){ 
+	/**
+	 * removeItem method is used to remove an item from the cart
+	 */
+	public void removeItem(String nameOfItem){ 
 		hashPrice.remove(nameOfItem);
 		hashQuantity.remove(nameOfItem);
 	}
 	
-	int billGenerate(){
+	/**
+	 * billGenerate method is used to return the total bill.
+	 */
+	public int billGenerate(){
 		int bill=0;
 		HashMap<String,Integer> hashBill =new HashMap<String, Integer>();
 		for(String key : hashPrice.keySet()){
