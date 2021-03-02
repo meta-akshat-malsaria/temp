5919b5b7e41fc46f60d998c9fd3e4b3eb1ac7354
Akshat Malsaria <akshat.malsaria@metacube.com>
2021-03-02 12:48:10 +0530

    Comments added as per Javadoc documentation.

--- Shopping/src/ShoppingCart.java
+++ Shopping/src/ShoppingCart.java
@@ -1,11 +1,18 @@
 import java.util.HashMap;
-
+/**
+ * This is the simple implementation of Shopping Cart.
+ * @author akshat.malsaria_meta
+ * @version 1.0
+ */
 public class ShoppingCart {
 	HashMap<String,Integer> hashPrice= new HashMap<String,Integer>();
 	HashMap<String,Integer> hashQuantity= new HashMap<String,Integer>();
 	
 	/**
 	 * addToCart method is used to add an item to the cart
+	 * @param nameOfItem Name of item added.
+	 * @param priceOfItem Price of item added.
+	 * @param quantityOfItem Quantity of item added.
 	 */
 	public void addToCart(String nameOfItem ,int priceOfItem, int quantityOfItem){
 		hashPrice.put(nameOfItem,priceOfItem);
@@ -14,13 +21,16 @@ public class ShoppingCart {
 	
 	/**
 	 * updateCart method is used to update on to the quantity of an individual item.
+	 * @param nameOfItem Name of item to be updated.
+	 * @param updatedQuantity Updated quantity of the item.
 	 */
 	public void updateCart(String nameOfItem,int updatedQuantity){
 		hashQuantity.replace(nameOfItem,updatedQuantity);
 	}
 	
 	/**
-	 * removeItem method is used to remove an item from the cart
+	 * removeItem method is used to remove an item from the cart.
+	 * @param nameOfItem Name of item to be removed.
 	 */
 	public void removeItem(String nameOfItem){ 
 		hashPrice.remove(nameOfItem);
@@ -29,6 +39,7 @@ public class ShoppingCart {
 	
 	/**
 	 * billGenerate method is used to return the total bill.
+	 * @return It returns final bill amount.
 	 */
 	public int billGenerate(){
 		int bill=0;
